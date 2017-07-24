cfb1b8a27eb6e94c2820661a24d78046b5bd7843
Marc Destefano <marc.destefano@gmail.com>
2017-07-24 10:23:41 -0400

    Tidied up input box on main menu

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -1,5 +1,5 @@
 /** @constructor */
-let spacebar, input;
+let spacebar, input, enter;
 
 Tetris.MainMenu = function() {};
 
@@ -11,15 +11,28 @@ Tetris.MainMenu.prototype.preload = function() {};
 
 Tetris.MainMenu.prototype.create = function() {
   this.stage.backgroundColor = 0x444444;
-  let text = Tetris.game.add.text(0, 0, "Dummy text, hit spacebar", {
+  Tetris.game.add.text(240, 50, "META-TWO Beta test", {
     font: "bold 32px Arial",
     fill: "#fff",
     boundsAlignH: "center",
     boundsAlignV: "middle"
   });
+  Tetris.game.add.text(190, 150, "Type in desired start level (default is 0)", {
+    font: "bold 24px Arial",
+    fill: "#fff",
+    boundsAlignH: "center",
+    boundsAlignV: "middle"
+  });
+  Tetris.game.add.text(210, 200, "Press Enter key or A button to begin", {
+    font: "bold 24px Arial",
+    fill: "#fff",
+    boundsAlignH: "center",
+    boundsAlignV: "middle"
+  });
   //text.setTextBounds(0, 100, 800, 100);
-  spacebar =  Tetris.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-  input = Tetris.game.add.inputField(10,90, {
+  //spacebar =  Tetris.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+  enter = Tetris.game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+  input = Tetris.game.add.inputField(375,300, {
     font: '18px Arial',
     fill: '#212121',
     fontWeight: 'bold',
@@ -28,17 +41,22 @@ Tetris.MainMenu.prototype.create = function() {
     borderWidth: 1,
     borderColor: '#000',
     borderRadius: 6,
-    placeHolder: '1',
+    placeHolder: '0',
     min: 0,
     max: 29,
-    type: PhaserInput.InputType.number
+    type: PhaserInput.InputType.number,
+    //blockInput: false
   });
-  input.setText(0);
+  input.blockInput = false;
+  //input.setText(0);
   input.startFocus();
-};
+  //console.log(input);
+}; // end create function
 
 Tetris.MainMenu.prototype.update = function() {
-  if (spacebar.isDown) {
+  if (enter.isDown) {
+    input.endFocus();
+    input.value = input.value===""?0:input.value;
     Tetris.config.startLevel = input.value;
     this.state.start(Tetris.Game.stateKey);
   }
